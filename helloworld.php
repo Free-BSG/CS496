44c4bf2b8046b892836284695fa7787f227a634a
Michael W Freeman <freemich@onid.oregonstate.edu>
2015-07-05 06:30:44 +0000

    Added message to see changes to page

--- helloworld.php
+++ helloworld.php
@@ -1,6 +1,6 @@
 <?php
   echo 'Hello, world!';
-
+	echo 'Michael is here';
  ?>
 <!DOCTYPE hrml PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <head>
